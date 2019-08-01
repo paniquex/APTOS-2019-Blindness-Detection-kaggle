fb4defb27b8ee7165536814e70d203348a9ccd62
Nikita <paniquexx@gmail.com>
2019-08-02 01:24:32 +0300

    Dataset class + transformer

---
+++ train_dataset.py
@@ -0,0 +1,31 @@
+from torch.utils.data.dataset import Dataset
+from torchvision.transforms import transforms
+import os
+import cv2
+
+transforms = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(p=0.4),
+    #transforms.ColorJitter(brightness=2, contrast=2),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+])
+
+class CreateDataset(Dataset):
+    def __init__(self, df_data, data_dir='./input/', transform=None):
+        super().__init__()
+        self.df = df_data.values
+        self.data_dir = data_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        img_name, label = self.df[index]
+        img_path = os.path.join(self.data_dir, img_name + '.png')
+        image = cv2.imread(img_path)
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, label
