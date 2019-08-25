b439737bba62c375f2c4efb36561bf1f3162b10b
Nikita <paniquexx@gmail.com>
2019-08-25 18:10:52 +0300

    clahe + Bulat crop were added

--- train_dataset.py
+++ train_dataset.py
@@ -5,7 +5,7 @@ import cv2
 from os.path import isfile
 import numpy as np
 from config import Config
-
+import math
 
 # The Code from: https://www.kaggle.com/ratthachat/aptos-updated-albumentation-meets-grad-cam
 cfg = Config()
@@ -49,6 +49,47 @@ def expand_path(p, train_path):
     return p
 
 
+
+def subtract_median_bg_image(im):
+    k = np.max(im.shape)//20*2+1
+    bg = cv2.medianBlur(im, k)
+    return cv2.addWeighted (im, 4, bg, -4, 128)
+
+PARAM = 96
+def Radius_Reduction(img,PARAM):
+    h,w,c=img.shape
+    Frame=np.zeros((h,w,c),dtype=np.uint8)
+    cv2.circle(Frame,(int(math.floor(w/2)),int(math.floor(h/2))),int(math.floor((h*PARAM)/float(2*100))), (255,255,255), -1)
+    Frame1=cv2.cvtColor(Frame, cv2.COLOR_BGR2GRAY)
+    img1 =cv2.bitwise_and(img,img,mask=Frame1)
+    return img1
+
+def info_image(im):
+    # Compute the center (cx, cy) and radius of the eye
+    cy = im.shape[0]//2
+    midline = im[cy,:]
+    midline = np.where(midline>midline.mean()/3)[0]
+    if len(midline)>im.shape[1]//2:
+        x_start, x_end = np.min(midline), np.max(midline)
+    else: # This actually rarely happens p~1/10000
+        x_start, x_end = im.shape[1]//10, 9*im.shape[1]//10
+    cx = (x_start + x_end)/2
+    r = (x_end - x_start)/2
+    return cx, cy, r
+
+def resize_image(im, image_size, augmentation=False):
+    # Crops, resizes and potentially augments the image to IMAGE_SIZE
+    cx, cy, r = info_image(im)
+    scaling = image_size/(2*r)
+    rotation = 0
+    if augmentation:
+        scaling *= 1 + 0.3 * (np.random.rand()-0.5)
+        rotation = 360 * np.random.rand()
+    M = cv2.getRotationMatrix2D((cx,cy), rotation, scaling)
+    M[0,2] -= cx - image_size/2
+    M[1,2] -= cy - image_size/2
+    return cv2.warpAffine(im,M,(image_size, image_size))
+
 class CreateDataset(Dataset):
 
     def __init__(self, df_data, data_dir, transform=None):
@@ -66,10 +107,14 @@ class CreateDataset(Dataset):
         p = self.df.id_code.values[idx]
         p_path = expand_path(p, self.train_path)
         image = cv2.imread(p_path)
-        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # image = crop_image_from_gray(image)
-        # image = cv2.resize(image, (cfg.img_size, cfg.img_size))
-        # image = cv2.addWeighted(image, 4, cv2.GaussianBlur(image, (0, 0), 30), -4, 128)
+        lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+        lab_planes = cv2.split(lab)
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16, 16))
+        lab_planes[0] = clahe.apply(lab_planes[0])
+        lab = cv2.merge(lab_planes)
+        clahed = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+        image = resize_image(clahed, cfg.img_size)
+
         image = transforms.ToPILImage()(image)
 
         if self.transform:
@@ -79,7 +124,8 @@ class CreateDataset(Dataset):
 
 transforms_train = transforms.Compose([
         transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation((-120, 120)),
+        transforms.RandomRotation((-15, 15)),
+        # transforms.RandomResizedCrop(cfg.img_size_crop),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
